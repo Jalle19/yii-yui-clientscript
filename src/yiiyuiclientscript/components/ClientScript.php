9deafda1df1351a6588e44da3ab74cff043ed034
Sam Stenvall <neggelandia@gmail.com>
2013-07-08 13:38:02 +0300

    compress inline scripts as well

--- src/yiiyuiclientscript/components/ClientScript.php
+++ src/yiiyuiclientscript/components/ClientScript.php
@@ -7,6 +7,7 @@
  */
 
 namespace yiiyuiclientscript\components;
+use yiiyuiclientscript\exceptions\Exception as Exception;
 
 class ClientScript extends \CClientScript
 {
@@ -75,7 +76,11 @@ class ClientScript extends \CClientScript
 	 */
 	private function combineScripts($position)
 	{
-		if ($this->enableJavaScript && isset($this->scriptFiles[$position]))
+		if (!$this->enableJavaScript)
+			return;
+
+		// Combine and compress script files
+		if (isset($this->scriptFiles[$position]))
 		{
 			if ($this->_javascriptCombiner === null)
 			{
@@ -86,6 +91,43 @@ class ClientScript extends \CClientScript
 			$this->scriptFiles[$position] = $this->_javascriptCombiner->
 					combine($this->scriptFiles[$position]);
 		}
+
+		// Combine inline scripts
+		$this->combineInlineScripts($position);
+	}
+
+	/**
+	 * Combines and compresses all inline scripts at the specified position. 
+	 * The combined contents is stored as a file in the assets folder so that 
+	 * the combination only happens when the contents change.
+	 * @param int $position the position of the scripts
+	 * @throws Exception if the combined script file can't be created
+	 */
+	private function combineInlineScripts($position)
+	{
+		if (!isset($this->scripts[$position]))
+			return;
+
+		$scriptHash = md5(serialize($this->scripts[$position]));
+		$combinedScript = \Yii::app()->assetManager->basePath
+				.'/'.$this->combinedScriptPrefix
+				.'-'.$scriptHash.'.js';
+
+		// Create a compressed version of the scripts if it doesn't exist
+		if (!file_exists($combinedScript))
+		{
+			$combiner = new Combiner($this->compressorOptions);
+
+			$contents = $combiner->compress(
+					\YUI\Compressor::TYPE_JS, $this->scripts[$position]);
+
+			if (@file_put_contents($combinedScript, $contents) === false)
+				throw new Exception('Failed to compress inline scripts: Could not write to file '.$combinedScript);
+		}
+
+		// Replace the originals
+		$this->scripts[$position] = array(
+			$scriptHash=>file_get_contents($combinedScript));
 	}
 
 }
